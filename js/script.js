93f66b93168ee120f15e4ed70d2f6fb44b40ac3c
SebastianCarai <sebastiancarai7@gmail.com>
2021-11-23 15:01:01 +0100

    crete boxes

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// ---------
+// FUNCTIONS
+// ---------
+function generateBoxes (){
+    gridContainer.innerHTML = '';
+    const value = document.getElementById('difficulty_select').value;
+    console.log(value); 
+
+    for (i=1; i<=value; i++){
+        const newBox = document.createElement('div');
+        newBox.classList.add('box');
+        newBox.innerHTML = `${i}`;
+        newBox.style.width = `calc(100% / ${Math.sqrt(value)})`;
+        newBox.style.height = `calc(100% / ${Math.sqrt(value)})`;
+        gridContainer.append(newBox);
+    } 
+}
+
+
+
+// ---------
+// MAIN
+// ---------
+const playBtn = document.getElementById('play_btn');
+const gridContainer = document.querySelector('.grid_container');
+
+playBtn.addEventListener('click', generateBoxes);
+
